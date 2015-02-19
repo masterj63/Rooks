eff5060c1d8eae8a71a99295e056b20bebb30cb1
Igor B <masterj63@gmail.com>
2015-02-19 21:23:31 +0400

    +inverseSort

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 class Sorter {
     static List<Integer>[] sort(byte[][][] a) {
-        int n = a.length;
+        final int n = a.length;
 
         @SuppressWarnings("unchecked")
         List<Integer>[] res = new ArrayList[n];
@@ -28,6 +30,21 @@ class Sorter {
                 if (!betweenExists)
                     res[i].add(j);
             }
+        }
+
+        return res;
+    }
+
+    static Set<Integer>[] inverseSort(List<Integer>[] sort) {
+        final int n = sort.length;
+
+        Set<Integer>[] res = new HashSet[n];
+        for (int i = 0; i < n; i++)
+            res[i] = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            for (Integer j : sort[i])
+                res[j].add(i);
         }
 
         return res;
